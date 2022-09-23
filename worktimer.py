0010954d2dcccebbdf583fcbd787ae8aecccda02
aaannestad <a.aannestad@gmail.com>
2022-09-22 22:40:22 -0500

    better layout

--- worktimer.py
+++ worktimer.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import tkinter
 from tkinter import ttk
 
 import pickle
@@ -16,19 +15,25 @@ data = {
        }
     },
     'clock': {
-        'clockedin?': FALSE,
+        'clockedin?': False,
         'time': 30
     }
 }
+
+def makebutton():
+    row = 1
+    for item in data['projects']:
+        ttk.Button(frm, text=data['projects'][item]['name']).grid(column=0,row=row)
+        ttk.Label(frm, text=data['projects'][item]['starttime']).grid(column=1, row=row)
+        row = row + 1
 
 root = Tk()
 
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 
-ttk.Label(frm, text=data['clock']['time']).pack()
+ttk.Label(frm, text=data['clock']['time']).grid(column=0, row=0, columnspan=2)
 
-for item in data['projects']:
-    ttk.Button(frm, text=data['projects'][item]['name']).pack(side=tkinter.LEFT)
+makebutton()
 
 root.mainloop()
